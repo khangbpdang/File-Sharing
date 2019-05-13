2d46356d1c064465125fee756e070c8a44327064
khangdang <khangdang@mail.adephi.edu>
2019-05-13 02:09:24 -0400

    Fix database connection and file path issue

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,9 @@
 <?php
+  //SESSION_START();
   /*
    * This file manages database connection and paths to uploads/profilepics folder
   */
   $conn = mysqli_connect('127.0.0.1', 'root', 'Overdrive08', 'mytestdb');
-  $pathToUpload='/Users/khangdang/Sites/uploads/'
-  $pathToProfile='/Users/khangdang/Sites/profilepics/'
+  $pathToUpload = '/Users/khangdang/Sites/uploads/';
+  $pathToProfile = '/Users/khangdang/Sites/profilepics/';
 ?>
